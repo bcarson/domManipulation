cc4ffe9bd8106393aaad0fb2075964b9a7ddc360
Bonnie Brennan <bjcarson75@yahoo.com>
2018-07-04 08:56:01 -0500

    initial commit

---
+++ src/app/my.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'my-component',
+  template: `
+  <br />
+    Hello from MyComponent!
+    ...<br />
+    ...<br />
+    Goodbye from MyComponent!
+    <br />
+    `,
+})
+export class MyComponent {}
